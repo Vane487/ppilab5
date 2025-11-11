6a08ca2efa4e4547cc38944aaf4ff1b8fcb6e970
Sophia <Sofia.Chuprina@ukr.net>
2025-11-11 20:52:20 +0200

    Add code for calculator.py

--- calculator.py
+++ calculator.py
@@ -12,4 +12,21 @@ def multiply(self, a, b):
         if b == 0:
             return "Помилка: ділення на нуль!"
         return a / b
+def power(self, a, b):
+        return a  b
+    
+    def square_root(self, a):
+        if a < 0:
+            return "Помилка: від'ємне число!"
+        return a  0.5
+
+# Тестовий код
+if name == "main":
+    calc = Calculator()
+    print("Додавання:", calc.add(5, 3))
+    print("Віднімання:", calc.subtract(5, 3))
+    print("Множення:", calc.multiply(5, 3))
+    print("Ділення:", calc.divide(6, 2))
+    print("Степінь:", calc.power(2, 3))
+    print("Корінь:", calc.square_root(9))
 
